7e5a500a20d98682c84e7b482f698e151ded8c46
Chris Wangsanata <chriswangsanata@gmail.com>
2023-09-26 21:44:23 -0700

    implement assignment

--- scripts/playground.js
+++ scripts/playground.js
@@ -8,37 +8,33 @@ const addButton = document.getElementById("add");
 // Part 3a:
 
 let count = 0;
-
 function increaseCount() {
   /*Increment the count variable by 1.*/
-  /* YOUR CODE HERE */
+  count++;
   countDisplay.textContent = count;
 }
 
-
 // Part 3b:
-
 /* Fill in the appropriate arguments for the addEventListener function. Reference the link in the spec for help! */
-addButton.addEventListener(/* YOUR CODE HERE */, /* YOUR CODE HERE */);
-
-
+addButton.addEventListener('click', increaseCount);
 
 // Part 3c:
-
 // Write subtractButton's event listener
-/* YOUR CODE HERE */
+subtractButton.addEventListener('click', () => {
+  count--;
+  countDisplay.textContent = count;
+});
 
 
 
 
 // Part 5:
-
 const inputTask = document.getElementById("input-task");
 const taskList = document.getElementById("tasks");
 // Initialize the const submitButton:
-/* YOUR CODE HERE */
+const submitButton = document.getElementById("submit");
 // Initialize the const clearButton:
-/* YOUR CODE HERE */
+const clearButton = document.getElementById("clear");
 
 
 
@@ -47,10 +43,11 @@ const taskList = document.getElementById("tasks");
 submitButton.addEventListener("click", () => {
   if (inputTask.value != "") {
       // Initialize a const variable named "task", and set it equal to a new li element. 
-      /* YOUR CODE HERE */
-      task.textContent = inputTask.value; // Here you set the li element you just created to have the text value in the input field!
+      const task = document.createElement('li');
+       // Here you set the li element you just created to have the text value in the input field!
+      task.textContent = inputTask.value;
       // Add this task to the end of taskList (this is the list you initialized earlier!).
-      taskList./* YOUR CODE HERE*/
+      taskList.appendChild(task);
       inputTask.value = "";
   }
 });
